b136bc199b79991170ae21b86f2d8783d8252b5d
Mojtaba2013 <133687853+Mojtaba2013@users.noreply.github.com>
2025-11-02 20:07:19 +0300

    Delete src directory

--- src/main.tsx/App.tsx
+++
@@ -1,7 +0,0 @@
-your-project/       <-- root of repo
-├─ src/             <-- folder for React/TSX code
-│  ├─       <-- entry point
-│  └─        <-- main component
-├─ index.html       <-- at root
-├─ package.json     <-- at root
-├─ vite.config.ts   <-- at root
